54afccc03b1bdfc58723c9a20e4f54ec39d93c36
CarsonBills <CarsonMBills@Gmail.com>
2015-07-16 12:48:09 -0400

    returns random author

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require("body-parser");
 var app = express();
-var today = 
+
 
 app.get("/", function(req, res){
 	res.render("index.ejs");
@@ -12,7 +12,8 @@ app.get("/poem", function(req, res){
 	request("http://poetrydb.org/author", function (error, response, body){
 		if (!error && response.statusCode === 200) {
 			var authors = JSON.parse(body).authors
-			res.locals = {authors: authors};
+			var author = authors[Math.floor(Math.random() * authors.length)];
+			res.locals = {author: author};
 			res.render("poem.ejs");
 		}
 	});
