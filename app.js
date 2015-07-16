7fae2c665f756c4830540f1d9e0909fb65511b70
CarsonBills <CarsonMBills@Gmail.com>
2015-07-16 12:37:37 -0400

    moves poems to another page

--- app.js
+++ app.js
@@ -2,28 +2,20 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require("body-parser");
 var app = express();
+var today = 
 
 app.get("/", function(req, res){
+	res.render("index.ejs");
+});
+
+app.get("/poem", function(req, res){
 	request("http://poetrydb.org/author", function (error, response, body){
 		if (!error && response.statusCode === 200) {
 			var authors = JSON.parse(body).authors
 			res.locals = {authors: authors};
-			res.render("index.ejs");
 		}
 	});
 });
-
-// var options = {
-// 	protocol: "http:",
-// 	host: "poetrydb.org",
-// 	pathname: "/author",
-// };
-
-// var searchURL = url.format(options);
-// console.log(searchURL);
-
-// app.get("/poetry", function(request, response))
-
 
 
 app.listen(8080);
