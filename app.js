341439734ee0f9cdc18c92ef2b5fed62dfbb678d
CarsonBills <CarsonMBills@Gmail.com>
2015-07-15 22:00:46 -0400

    returns list of authors

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var express = require('express');
+var request = require('request');
+var bodyParser = require("body-parser");
 var app = express();
 
-app.get("/", function(request, response){
-	response.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res){
+	request("http://poetrydb.org/author", function (error, response, body){
+		if (!error && response.statusCode === 200) {
+			var authors = JSON.parse(body).authors
+			res.locals = {authors: authors};
+			res.render("index.ejs");
+		}
+	});
 });
+
+// var options = {
+// 	protocol: "http:",
+// 	host: "poetrydb.org",
+// 	pathname: "/author",
+// };
+
+// var searchURL = url.format(options);
+// console.log(searchURL);
+
+// app.get("/poetry", function(request, response))
+
+
 
 app.listen(8080);
