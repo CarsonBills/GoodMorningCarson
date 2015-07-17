e7faf065beafa78ad6a108f75ec2c4b0ce013cc6
CarsonBills <CarsonMBills@Gmail.com>
2015-07-17 11:33:45 -0400

    defines and calls updateClock method

--- public/lib/scripts.js
+++ public/lib/scripts.js
@@ -36,3 +36,17 @@ function printPoem(poemData){
 function printWeather(weatherData){
 	$(".content").append("<p>" + weatherData.city + "</p>");
 }
+
+function updateClock ( ){
+  var currentTime = new Date ( );
+  var currentHours = currentTime.getHours ( );
+  var currentMinutes = currentTime.getMinutes ( );
+  var currentSeconds = currentTime.getSeconds ( );
+
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+   
+  $("#clock").html(currentTimeString);          
+}
