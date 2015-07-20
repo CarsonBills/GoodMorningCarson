465e72a36de4720998b4c1c912942a41a11c519b
CarsonBills <CarsonMBills@Gmail.com>
2015-07-20 17:53:27 -0400

    angular prints out example poem

--- public/lib/scripts.js
+++ public/lib/scripts.js
@@ -3,98 +3,112 @@ console.log(":)")
 
 var app = angular.module("gmc", []);
 
-function getPoem(){
-	$.ajax({
-		url : "/poem",
-		type: "get",
-		dataType: "json",
-		success: function(data) {
-			console.log(data)
-			printPoem(data);
-		}
-	});
-}
+app.controller("PoemController", function(){
+	this.poem = poemData;
+});
 
-function getWeather(){
-	$.ajax({
-		url : "/weather",
-		type: "get",
-		success: function(data) {
-			console.log(data);
-			printWeather(data);
-		}
-	});
-}
-
-function getHistory(){
-	$.ajax({
-		url: "/history",
-		type: "get",
-		success: function(data) {
-			console.log(data);
-		}
-	});
-}
-
-function getCute(){
-	$.ajax({
-		url: "/cute",
-		type: "get",
-		success: function(data) {
-			console.log(data);
-		}
-	});
-}
-
-function getRedditLinks(){
-	$.ajax({
-		url: "/reddit",
-		type: "get",
-		success: function(data){
-			console.log(data);
-		}
-	});
-}
-
-function getNYTLinks(){
-	$.ajax({
-		url: "/nyttop",
-		type: "get",
-		success: function(data){
-			console.log(data);
-		}
-	});
-}
-
-function printNYT(){
-
-}
-
-function printReddit(){
-
-}
-
-function printHistory(){
-
-}
-
-function printCute(){
-
-}
-
-function printPoem(poemData){
-	$("h2.section").html(poemData.sectionTitle)
-	$("h2.author").html(poemData.author);
-	$("h3.title").html(poemData.poemTitle);
-	$.each(poemData.lines, function(i, val){
-		$(".lines").append("<p>" + val + "</p>");
-	})
+var poemData = {
+	title: "Poem Title",
+	author: "Poem Author",
+	lines: ["To be or not to be", "that is the question"]
 }
 
 
-function printWeather(weatherData){
-	$(".content").append("<p>" + weatherData.city + "</p>");
-}
+
+
+
+// function getPoem(){
+// 	$.ajax({
+// 		url : "/poem",
+// 		type: "get",
+// 		dataType: "json",
+// 		success: function(data) {
+// 			console.log(data)
+// 			printPoem(data);
+// 		}
+// 	});
+// }
+
+// function getWeather(){
+// 	$.ajax({
+// 		url : "/weather",
+// 		type: "get",
+// 		success: function(data) {
+// 			console.log(data);
+// 			printWeather(data);
+// 		}
+// 	});
+// }
+
+// function getHistory(){
+// 	$.ajax({
+// 		url: "/history",
+// 		type: "get",
+// 		success: function(data) {
+// 			console.log(data);
+// 		}
+// 	});
+// }
+
+// function getCute(){
+// 	$.ajax({
+// 		url: "/cute",
+// 		type: "get",
+// 		success: function(data) {
+// 			console.log(data);
+// 		}
+// 	});
+// }
+
+// function getRedditLinks(){
+// 	$.ajax({
+// 		url: "/reddit",
+// 		type: "get",
+// 		success: function(data){
+// 			console.log(data);
+// 		}
+// 	});
+// }
+
+// function getNYTLinks(){
+// 	$.ajax({
+// 		url: "/nyttop",
+// 		type: "get",
+// 		success: function(data){
+// 			console.log(data);
+// 		}
+// 	});
+// }
+
+// function printNYT(){
+
+// }
+
+// function printReddit(){
+
+// }
+
+// function printHistory(){
+
+// }
+
+// function printCute(){
+
+// }
+
+// function printPoem(poemData){
+// 	$("h2.section").html(poemData.sectionTitle)
+// 	$("h2.author").html(poemData.author);
+// 	$("h3.title").html(poemData.poemTitle);
+// 	$.each(poemData.lines, function(i, val){
+// 		$(".lines").append("<p>" + val + "</p>");
+// 	})
+// }
+
+
+// function printWeather(weatherData){
+// 	$(".content").append("<p>" + weatherData.city + "</p>");
+// }
 
 function updateClock ( ){
   var currentTime = new Date ( );
