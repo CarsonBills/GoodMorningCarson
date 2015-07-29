4ec5bedcb373f29e31f5f1d7b97ff6d1d9c3b379
CarsonBills <CarsonMBills@Gmail.com>
2015-07-29 13:26:48 -0400

    adjust NYT logo

--- public/lib/scripts.js
+++ public/lib/scripts.js
@@ -127,17 +127,3 @@ app.directive("weatherData", ["$http", function($http){
 function clearLoading(){
 	$(".loading").hide();
 }
-
-function updateClock ( ){
-  var currentTime = new Date ( );
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
-
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
-   
-  $("#clock").html(currentTimeString);          
-}
